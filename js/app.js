8c25f513859ba0c39627ba4c2dfc9255c7b6c7e1
Martin Gonzalez <martineluney023@gmail.com>
2023-04-27 17:56:34 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -27,6 +27,9 @@ let header = document.querySelector('header');
 let productos = document.querySelector('.trending-product');
 let h2 = document.querySelector('.center-text');
 let container = document.querySelector('body');
+let clientes = document.querySelector('.client-reviews');
+let contacto = document.querySelector('.contact');
+let footer = document.querySelector('.end-text');
 
 let mode = "light"
 
@@ -36,7 +39,10 @@ function lightMode(){
     header.className = 'header';
     productos.className = 'trending-product';
     h2.className = 'center-text h2';
-    container.className = 'body'
+    container.className = 'body';
+    clientes.className = 'client-reviews';
+    contacto.className = 'contact';
+    footer.className = 'end-text';
 }
 
 function darkMode(){
@@ -46,6 +52,9 @@ function darkMode(){
     productos.className = 'dark-trending-product';
     h2.className = 'dark-center-text h2';
     container.className = 'dark-body';
+    clientes.className = 'dark-client-reviews';
+    contacto.className = 'dark-contact';
+    footer.className - 'dark-end-text';
 
 }
 
@@ -60,52 +69,6 @@ circulo.addEventListener('click', () =>{
 })
 
 
-/*
-const productos = [
-    {
-        id: 'Vestido-01',
-        nombre: 'Living Dolls Dress',
-        precio: 50000
-    },
-    {
-        id: "Vestido-02",
-        nombre: 'Prudence Dress',
-        precio: 90000
-    },
-    {
-        id: 'Vestido-03',
-        nombre: 'Moon Dress',
-        precio: 40000
-    },
-    {
-        id: 'Vestido-04',
-        nombre: 'Black Magic Dress',
-        precio: 50000
-    },
-    {
-        id: 'Vestido-05',
-        nombre: 'Arcana Dress',
-        precio: 50000
-    },
-    {
-        id: 'Vestido-06',
-        nombre: 'Shadow Forest Dress',
-        precio: 80000
-    },
-    {
-        id: 'Vestido-07',
-        nombre: 'Monroe Dress',
-        precio: 60000
-    },
-    {
-        id: 'Vestido-08',
-        nombre: 'Minerva Dress',
-        precio: 35000
-    }
-    
-    
-];
-
 
 
 // Función para solicitar al usuario un producto por su ID
@@ -114,7 +77,7 @@ let carrito = [];
 function solicitarProducto() {
 
     let productoSeleccionado = prompt('Ingrese el ID del producto que desea')
-    productoSeleccionado = productos.find((producto) => producto.id === productoSeleccionado);
+    productoSeleccionado = producto.find((producto) => producto.id === productoSeleccionado);
 
     if(!productoSeleccionado){ // comprueba si el valor es falsy, es decir, si es null, undefined, 0, '', false, o NaN. Si productoSeleccionado es falsy, significa que el ID de producto ingresado no es válido, por lo que se muestra una alerta al usuario indicando que el ID es inválido y la función retorna null. 
         productoseleccionado = prompt('El ID ingresado es invalido, por favor vuelva a ingresar un ID valido');
@@ -128,14 +91,14 @@ const cargarProductos = () =>{
     let seguir;
     let total = 0;
     do{
-        const producto = solicitarProducto(); // para poder obtener el objeto producto que se seleccionó y luego poder agregarlo al carrito de compras
+        const producto1 = solicitarProducto(); // para poder obtener el objeto producto que se seleccionó y luego poder agregarlo al carrito de compras
         seguir = prompt('Desea ingresar otro producto si/no');
 
         if(seguir !== 'si' && seguir !== 'no'){
         seguir = prompt('Error, debe ingresar si o no');
         }
 
-        if (producto && seguir === 'no'){ //El parámetro producto se refiere al resultado devuelto por la función solicitarProducto(), verifica si el usuario ha ingresado productos antes de decidir no ingresar mas
+        if (producto1 && seguir === 'no'){ //El parámetro producto se refiere al resultado devuelto por la función solicitarProducto(), verifica si el usuario ha ingresado productos antes de decidir no ingresar mas
             total = sumarProductos(carrito)
             alert(`El total de los productos seleccionados es: $${total}`);
         }
@@ -159,4 +122,4 @@ function sumarProductos(productos) {
 
 // Ejecutar el programa
 cargarProductos()
-*/
+
---
+++ js/items.js
@@ -0,0 +1,23 @@
+class producto {
+    constructor(id, nombre, precio){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+    }
+}
+
+const vestido1 = new producto ('Vestido-01', 'Arcana Dress', 50000);
+const vestido2 = new producto ('Vestido-02', 'Shadow Forest Dress', 80000);
+const vestido3 = new producto ('Vestido-03', 'Monroe Dress', 60000);
+const vestido4 = new producto ('Vestido-04', 'Living Dolls Dress', 50000);
+const vestido5 = new producto ('Vestido-05', 'Prudence Dress', 90000);
+const vestido6 = new producto ('Vestido-06', 'Moon Dress', 40000);
+const vestido7 = new producto ('Vestido-07', 'Black Magic Dress', 50000);
+const vestido8 = new producto ('Vestido-08', 'Minerva Dress', 35000);
+const vestido9 = new producto ('Vestido-09', 'Darkfall Dress', 26600);
+const vestido10 = new producto ('Vestido-10', 'Shivers Dress', 28000);
+const vestido11 = new producto ('Vestido-11', 'Lifeia Dress', 27500);
+const vestido12 = new producto ('Vestido-12', 'Naenia Dress', 33000);
+const vestido13 = new producto ('Vestido-13', 'Orades Dress', 21500);
+const vestido14 = new producto ('Vestido-14', 'Ghost Dress', 46000);
+const vestido15 = new producto ('Vestido-15', 'Angel Dress', 39000);
