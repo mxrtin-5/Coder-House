734b3a705a448e48e935278c993b1c7dd7922b16
mxrtin-5 <martineluney023@gmail.com>
2023-05-15 11:16:14 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -153,18 +153,11 @@ Total compra: $${total}
 
 };
 
-async function swalPagar() {
-    const { value: email } = await Swal.fire({
-        title: 'Input email address',
-        input: 'email',
-        inputLabel: 'Your email address',
-        inputPlaceholder: 'Enter your email address'
-    })
-
-    if (email) {
-        Swal.fire(`Entered email: ${email}`)
-    }
+function swalPagar() {
+    location.href='sesion.html'
 }
+
+
 
 const eliminarProducto = (product) => {
     const indice = carrito.findIndex(producto => producto.id === product.id);
